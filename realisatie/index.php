e562b48cf4ef8fa2586967c9b147cbd4d12a71f2
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-20 11:15:29 +0200

    update index.php, style.css, bestand voor js

---
+++ realisatie/index.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+
+	require("includes/connection.php");
+
+	$id = $_SESSION['id'];
+
+	if (isset($id)) {
+		$_head = 'pages/userHeader';
+	} else {
+		$_head = 'pages/header'; 
+    }
+    
+	require($_head.".php");
+
+	if(isset($_GET['page'])){
+
+		$pages=array("home","dashboard","aanmelden","account","register");
+
+		if(in_array($_GET['page'], $pages)) {
+
+			$_page=$_GET['page'];
+
+		}else{
+
+			$_page="home";
+
+		}
+	}
+?>
+<div class="wrapper">
+	<?php require("pages/".$_page.".php"); ?>
+</div>
+
+<?php include 'pages/footer.php';?>
