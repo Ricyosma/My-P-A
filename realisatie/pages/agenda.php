c367d56e13dbf7e8b85b1aa610cc65e9e96a2a4b
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-20 15:24:20 +0200

    update

--- realisatie/pages/agenda.php
+++ realisatie/pages/agenda.php
@@ -1,11 +1,25 @@
 <section>
    <div class="grid-container">
-        <div class="Monday">test</div>
-        <div class="Tuesday"></div>
-        <div class="Wednesday"></div>
-        <div class="Thuersday"></div>
-        <div class="Friday"></div>
-        <div class="Saturday"></div>
-        <div class="Sunday"></div>
+        <div class="Monday lane">
+            <h4>Monday</h4>            
+        </div>
+        <div class="Tuesday lane">
+            <h4>Tuesday</h4>
+        </div>
+        <div class="Wednesday lane">
+            <h4>Wednesday</h4>
+        </div>
+        <div class="Thursday lane">
+            <h4>Thursday</h4>
+        </div>
+        <div class="Friday lane">
+            <h4>Friday</h4>
+        </div>
+        <div class="Saturday lane">
+            <h4>Saturday</h4>
+        </div>
+        <div class="Sunday lane">
+            <h4>Sunday</h4>
+        </div>
     </div>
 </section>
