aabfb97748aa54de59c3e0268ca9b2be87f647b2
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-20 14:24:32 +0200

    update

--- realisatie/pages/agenda.php
+++ realisatie/pages/agenda.php
@@ -1,6 +1,6 @@
 <section>
    <div class="grid-container">
-        <div class="Monday"></div>
+        <div class="Monday">test</div>
         <div class="Tuesday"></div>
         <div class="Wednesday"></div>
         <div class="Thuersday"></div>
@@ -10,5 +10,4 @@
     </div>
 </section>
 <?php 
-
     echo 'welkom';
