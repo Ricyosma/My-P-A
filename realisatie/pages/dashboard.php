2d99ba706cf8287b5fb3e1e355c978ca275d2ec3
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-21 10:06:26 +0200

    update

---
+++ realisatie/pages/dashboard.php
@@ -0,0 +1,24 @@
+
+<?php 
+
+    include 'pages/agenda.php';
+    
+?>
+<section id="dashboard">
+    <div id="dashControls">
+        <div class="dashInfo">
+            <h2 class="title">Task name</h2>
+        </div>
+        <div class="dashInfo">
+            <h2 class="title">Time</h2>
+
+        </div>
+        <div class="dashInfo">
+            <h2 class="title">Priority</h2>
+        </div>
+        <div id="discription">
+            <h2>Description</h2>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+    </div>
+</section>
