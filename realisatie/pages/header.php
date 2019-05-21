c4b5cfcd8ffde5ed9142eb5c9213130b8ea77522
LukasSebastiaan <wevers.luke@gmail.com>
2019-05-21 15:40:05 +0200

    update

--- realisatie/pages/header.php
+++ realisatie/pages/header.php
@@ -23,9 +23,6 @@
     <link rel="stylesheet" href="css/header-footer.css">
     <link rel="stylesheet" href="css/style.css">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark">
