b4b8fefea9b46c78dd67d511d079072ab235ad82
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-21 14:35:19 +0200

    update

--- realisatie/index.php
+++ realisatie/index.php
@@ -17,7 +17,7 @@
 
 	if(isset($_GET['page'])){
 
-		$pages=array("home","agenda","dashboard","aanmelden","account","register");
+		$pages=array("home","agenda","dashboard","login","account","register");
 
 		if(in_array($_GET['page'], $pages)) {
 
--- realisatie/pages/login.php
+++ realisatie/pages/login.php
@@ -1,36 +1,36 @@
 <?php
-require '../includes/connection.php';
-if(isset($_POST['submit'])){
-$user = $ww = $pass = '';
-$user = $_POST['email'];
-$ww =  $_POST['password'];
-$pass = md5($ww);
-if(empty($user) || empty($pass)) {
-	$message = 'All field are required';
-} else {
-	$query = $conn->prepare("SELECT * FROM user WHERE E_mail=? AND passw=?");
-	$query->execute(array($user,$pass));
-	$row = $query->fetch(PDO::FETCH_BOTH);
-	if($query->rowCount() > 0) {
-		$_SESSION['E_mail'] = $user;
-		$id = $row["user_ID"];
-		$vnaam = $row["First_name"];
-		$anaam = $row['Last_name'];
-		session_start();
-		$_SESSION['id'] = $id;
-		$_SESSION['anaam'] = $anaam;
-		$_SESSION['vnaam'] = $vnaam;
-		header("Location: ../index.php?page=home");
+	require '../includes/connection.php';
+	if(isset($_POST['submit'])){
+	$user = $ww = $pass = '';
+	$user = $_POST['email'];
+	$ww =  $_POST['password'];
+	$pass = md5($ww);
+	if(empty($user) || empty($pass)) {
+		$message = 'All field are required';
 	} else {
-		$message = "Username/Password is wrong";
+		$query = $conn->prepare("SELECT * FROM user WHERE E_mail=? AND passw=?");
+		$query->execute(array($user,$pass));
+		$row = $query->fetch(PDO::FETCH_BOTH);
+		if($query->rowCount() > 0) {
+			$_SESSION['E_mail'] = $user;
+			$id = $row["user_ID"];
+			$vnaam = $row["First_name"];
+			$anaam = $row['Last_name'];
+			session_start();
+			$_SESSION['id'] = $id;
+			$_SESSION['anaam'] = $anaam;
+			$_SESSION['vnaam'] = $vnaam;
+			header("Location: ../index.php?page=home");
+		} else {
+			$message = "Username/Password is wrong";
+		}
 	}
-}
-}
+	}
 ?>
 <form action="" id="aan-form" method="post">
-			<div class="form-group">
-<?php if(isset($_POST['submit'])){ echo $message;{ ?>
-			</div>
+		<div class="form-group">
+			<?php if(isset($_POST['submit'])){ echo $message;{ ?>
+		</div>
 	    <div class="form-group">
 	      <label for="exampleInputEmail2">Emailadres</label>
 	      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Vul uw email in" require name="email">
@@ -40,4 +40,4 @@ if(empty($user) || empty($pass)) {
 	      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Uw wachtwoord" require name="password">
       </div>
 	    <button type="submit" name="submit" class="btn-sub btn btn-primary">Aanmelden</button>
-  </form>
+</form>
