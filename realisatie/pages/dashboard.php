f3d62153db03c0fe9fd2bfe81488bcdeae0180b2
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-21 15:37:24 +0200

    update

--- realisatie/pages/dashboard.php
+++ realisatie/pages/dashboard.php
@@ -24,30 +24,23 @@
     <form action="" method="post">
         <div id="dashControls">
             <div class="dashInfo">
-                <h2 class="title">Task name</h2>
+                <h3 class="title">Task name</h3>
                 <input type="text" name="taskName" id="taskName" placeholder="">
             </div>
             <div class="dashInfo">
-                <h2 class="title">Time</h2>
+                <h3 class="title">Time</h3>
                 <input type="time" name="dash" id="taskTime">
             </div>
             <div class="dashInfo">
-                <h2 class="title">Priority</h2>
-                <div class="star-rating">
-                    <span class="far fa-circle" data-rating="1"></span>
-                    <span class="far fa-circle" data-rating="2"></span>
-                    <span class="far fa-circle" data-rating="3"></span>
-                    <span class="far fa-circle" data-rating="4"></span>
-                    <span class="far fa-circle" data-rating="5"></span>
-                    <input type="hidden" name="whatever1" class="rating-value" value="2.56">
-                </div>
+                <h3 class="title">Priority</h3>
+                <input type="range" name="range" id="" min="0" max="5">
             </div>
             <div id="discription">
-                <h2>Description</h2>
+                <h3>Description</h3>
                 <textarea name="discription" id="taskDisc" rows="8"></textarea>
             </div>
             <div class="dashInfo">
-                
+                <h3>Repeat</h3>
             </div>
             <button id="goButton" type="submit">Go</button>
             <button id="stopButton">Cancel</button>
