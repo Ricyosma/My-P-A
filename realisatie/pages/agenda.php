7871cb9d1128cef1f8726d9d99cfa6b816b0e61d
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-22 20:21:39 +0200

    update
    het posten van de task in de correcte lane

--- realisatie/pages/agenda.php
+++ realisatie/pages/agenda.php
@@ -2,42 +2,46 @@
    <div class="grid-container">
         <?php 
             $id = $_SESSION['id'];
-
-            $taskQuery = $conn->prepare("SELECT task FROM task where User_ID=?");
-            $taskQuery->execute(array($id));
-            $result = $taskQuery->setFetchMode(PDO::FETCH_ASSOC); 
-            if($taskQuery->rowCount() > 0) {
-                if($row = $taskQuery->fetch(PDO::FETCH_ASSOC)){
-                    // $date = $row['Date'];
-                    $currentDate = date("Y/m/d");
-                    $startdate = strtotime($currentDate);
-                    $startdate = strtotime("-1 day");
-                    $enddate = strtotime("+7 days", $startdate);
-                    while ($startdate < $enddate) {
-                        $startdate = strtotime("+1 day", $startdate);
+            $day = date('w');
+            $day = $day - 1;
+            $week_start = date("Y/m/d", strtotime('-'.$day.' days'));
+            $startdate = strtotime($week_start);
+            $enddate = strtotime("+7 days", $startdate);
+            while ($startdate < $enddate) {
 ?>
-                        <div class="day">
-                            <h4><?php  echo date("l", $startdate);?></h4> 
-                            <div class="<?php echo date("l", $startdate);?> lane">
+                <div class="day">
+                    <h4>
+                        <?php  
+                            echo date("l", $startdate);
+                            ?>
+                            <br>
+                            <?php
+                            echo date("Y/m/d", $startdate);
+                        ?>
+                    </h4> 
+                    <div class="<?php echo date("l", $startdate);?> lane">
 <?php 
-                            $dayQuery = $conn->prepare("SELECT * FROM task WHERE Date=?");
-                            $dayQuery->execute(array(date("Y/m/d", $startdate)));
+                            $dayQuery = $conn->prepare("SELECT * FROM task WHERE Date=? and User_ID = ?");
+                            $dayQuery->execute(array(date("Y/m/d", $startdate),$id));
                             $result2 = $dayQuery->setFetchMode(PDO::FETCH_ASSOC); 
                             if($dayQuery->rowCount() > 0) {
-                                if($row2 = $dayQuery->fetch(PDO::FETCH_ASSOC)){
+                                while($row = $dayQuery->fetch(PDO::FETCH_ASSOC)){
                                     echo $row['Task'];
-                                    
+                                    echo $row['Date'];
+                                    echo $row['Description'];
                                 }
-                            }
+                            } 
+                        $startdate = strtotime("+1 day", $startdate);
 ?>
                             </div>
                         </div>
                         <?php
-                    }
                 }
-            } else {
-                echo 'fail' ;
-            }
+                // }
+            // } 
+            // else {
+            //     echo 'fail' ;
+            // }
 ?>
     </div>
 </section>
