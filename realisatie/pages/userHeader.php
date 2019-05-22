5e44d994f681caf8fed1c83dedf44f4e1dd752f3
Nieyam <nieyambusiness@gmail.com>
2019-05-22 10:38:12 +0200

    .

--- realisatie/pages/userHeader.php
+++ realisatie/pages/userHeader.php
@@ -42,9 +42,8 @@
           <li class="dropdown">
             <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
             <div class="dropdown-content">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <a href="#">Profiel</a>
+              <a href="#">Uitloggen</a>
             </div>
           </li>
         </ul>
