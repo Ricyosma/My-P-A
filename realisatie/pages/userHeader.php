bbda07b41a34afb7b3f87bd6c2ac5b2f01d31a71
LukasSebastiaan <wevers.luke@gmail.com>
2019-05-22 09:59:06 +0200

    update

--- realisatie/pages/userHeader.php
+++ realisatie/pages/userHeader.php
@@ -26,25 +26,23 @@
      <link rel="stylesheet" href="css/agenda.css">
     <link rel="stylesheet" href="css/dashboard.css">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark">
+  <nav>
         <a class="navbar-brand" href="index.php?page=home">My P-A</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="dropdown-item" href="index.php?page=agenda">Agenda</a>
+      <div>
+        <ul>
+          <li>
+            <a href="index.php?page=agenda">Agenda</a>
           </li>
-          <li class="nav-item">
-            <a class="dropdown-item" href="index.php?page=dashboard">Dashboard</a>
+          <li>
+            <a href="index.php?page=dashboard">Dashboard</a>
           </li>
-          <li class="nav-item">
-            <a class="dropdown-item" href="index.php?page=account">Account</a>
+          <li>
+            <a href="index.php?page=account">Account</a>
           </li>
           <!-- Dropdown -->
             <ul>
