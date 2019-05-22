511a251802b2f0e3b69a20d3447e16ff6ab7c6a0
LukasSebastiaan <wevers.luke@gmail.com>
2019-05-22 10:35:14 +0200

    update

--- realisatie/pages/userHeader.php
+++ realisatie/pages/userHeader.php
@@ -39,15 +39,14 @@
           <li>
             <a href="index.php?page=dashboard">Dashboard</a>
           </li>
-          <!-- Dropdown -->
-          <div class="dropdown">
-            <button class="dropbtn">Account</button>
+          <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
             <div class="dropdown-content">
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
               <a href="#">Link 3</a>
             </div>
-          </div>
+          </li>
         </ul>
       </div>
     </nav>
